63607846c5c395b5d1ac8aa30a4014c45463d7ff
Martin <maritn.muriuki@strathmore.edu>
2022-02-18 17:30:07 +0300

    Calculate score using conditional statements

--- lib/result.dart
+++ lib/result.dart
@@ -3,10 +3,37 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhase {
+    String resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = "You are an Innocent Soul!";
+    } else if (resultScore <= 12) {
+      resultText = "Pretty Likeable!";
+    } else if (resultScore <= 16) {
+      resultText = 'The world has tainted your soul';
+    } else {
+      resultText = 'No redemption!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
-       );
+      // child: Column(
+      //   children: [ 
+      //     Text(
+      //       resultPhase,
+      //       style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      //       textAlign: TextAlign.center,
+      //     ),
+      //     FlatButton(child: Text('Retake Quiz'))
+      //   ],
+      // ),
+    );
   }
 }
