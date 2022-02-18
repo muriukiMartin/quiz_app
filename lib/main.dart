9280acad2ed100d65dd9239288f8887096a14a37
Martin <maritn.muriuki@strathmore.edu>
2022-02-18 17:29:21 +0300

    Attach score to answer

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,42 @@ class MyQuizApp extends StatefulWidget {
 }
 
 class _MyQuizAppState extends State<MyQuizApp> {
-  var _questionIndex = 0;
-
   final _questions = const [
     {
       'questionText': 'What\'s your favourite color?',
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ]
     },
     {
       'questionText': 'What\'s your favourite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ]
     },
     {
       'questionText': 'Who\'s your favourite instructor?',
-      'answers': ['John', 'Jane', 'Joe', 'Joan']
+      'answers': [
+        {'text': 'John', 'score': 1},
+        {'text': 'Jane', 'score': 1},
+        {'text': 'Joe', 'score': 1},
+        {'text': 'Joan', 'score': 1},
+      ]
     },
   ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -60,7 +78,7 @@ class _MyQuizAppState extends State<MyQuizApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
